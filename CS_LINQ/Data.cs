2fb898a2e45d7a2fd5a8b5192ed00a3a2beba610
maheshsabnis <sabnis_m@hotmail.com>
2022-07-28 12:49:25 +0530

    Date:28-July-2022

---
+++ CS_LINQ/Data.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_LINQ
+{
+    public class Employee
+    {
+        public int EmpNo { get; set; }
+        public string EmpName { get; set; }
+        public string DeptName { get; set; }
+        public int Salary { get; set; }
+    }
+    public class EmployeeCollection : List<Employee>
+    {
+        public EmployeeCollection()
+        {
+            Add(new Employee() { EmpNo = 1001, EmpName = "YashodaNandan", DeptName = "IT", Salary = 53000 });
+            Add(new Employee() { EmpNo = 1002, EmpName = "DevkiNandan", DeptName = "CTD", Salary = 33000 });
+            Add(new Employee() { EmpNo = 1003, EmpName = "RadheShyam", DeptName = "SYS", Salary = 63000 });
+            Add(new Employee() { EmpNo = 1004, EmpName = "Gopal", DeptName = "HRD", Salary = 13000 });
+            Add(new Employee() { EmpNo = 1005, EmpName = "Govind", DeptName = "SYS", Salary = 93000 });
+            Add(new Employee() { EmpNo = 1006, EmpName = "Mohan", DeptName = "CTD", Salary = 63000 });
+            Add(new Employee() { EmpNo = 1007, EmpName = "Madhav", DeptName = "IT", Salary = 23000 });
+            Add(new Employee() { EmpNo = 1008, EmpName = "Milind", DeptName = "ACCTS", Salary = 53000 });
+            Add(new Employee() { EmpNo = 1009, EmpName = "Vasudev", DeptName = "ACCTS", Salary = 63000 });
+            Add(new Employee() { EmpNo = 1010, EmpName = "Shridhar", DeptName = "IT", Salary = 83000 });
+        }
+    }
+}
---
+++ CS_LINQ/Program.cs
@@ -0,0 +1,83 @@
+﻿using CS_LINQ;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Using LINQ");
+
+
+var person = new { PersonId = 1, PersonName = "MAhesh" };
+
+
+EmployeeCollection employees = new EmployeeCollection();
+
+//foreach (var item in employees)
+//{
+//    if (item.DeptName == "IT" && item.EmpName.StartsWith('Y'))
+//    {
+//        Console.WriteLine(item.EmpName);
+//    }
+//}
+
+// Retrieve only those employees having DeptName as IT
+var EmpByDname = employees.Where(emp => emp.DeptName == "IT");
+Print(EmpByDname);
+
+Console.WriteLine();
+var EmpInSortedByEmpame = EmpByDname.OrderBy(emp=>emp.EmpName);
+Print(EmpInSortedByEmpame);
+Console.WriteLine();
+var EmpInSortedByEmpameDesc = EmpByDname.OrderByDescending(emp => emp.EmpName);
+
+var FInalResult = employees.Where(emp => emp.DeptName == "IT").OrderBy(emp => emp.EmpName);
+
+
+// USing the LINQ Query Approach
+
+var empByDeptName = from emp in employees
+                    where emp.DeptName == "IT"
+                    select emp;
+
+Print(empByDeptName);
+Console.WriteLine();
+
+
+var empByDeptNameOrder = from emp in employees
+                    where emp.DeptName == "IT"
+                    orderby emp.EmpName ascending
+                    select emp;
+
+
+Print(empByDeptNameOrder);
+
+
+Console.WriteLine("");
+Console.WriteLine("Sum of Salary Group by DeptName");
+
+var sumlgroupbydeptname = (from emp in employees
+                          // Group on DeptNAme ponuted by the deptgroup
+                          group emp by emp.DeptName into deptgroup
+                          // selecting each record from each group to calculate Sum of salary
+                          // and stored it in Anonymous Type
+                          select new
+                          {
+                              DeptName = deptgroup.Key, // The Group Name
+                              Salary = deptgroup.Sum(e=>e.Salary)
+                          }).ToList(); // COnvert the result into List
+
+
+foreach (var item in sumlgroupbydeptname)
+{
+    Console.WriteLine($"DeptName = {item.DeptName} and Salary = {item.Salary}");
+}
+
+
+static void Print(IEnumerable<Employee> emps)
+{
+    foreach (var item in emps)
+    {
+        Console.WriteLine(item.EmpName);
+    }
+}
+
+
+
+
+Console.ReadLine(); 
