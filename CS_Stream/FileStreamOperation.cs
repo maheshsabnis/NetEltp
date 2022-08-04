a8ddeb8749751324313a5b6c5363158abf6da3dd
maheshsabnis <sabnis_m@hotmail.com>
2022-08-04 10:02:16 +0530

    Date:04-AUg-2022

---
+++ CS_Stream/FileStreamOperation.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace CS_Stream
+{
+    public class FileStreamOperation : IDisposable
+    {
+        FileStream fs;
+        string filePath = string.Empty;
+        public FileStreamOperation()
+        {
+            filePath = @"c:\Coditas\Files\MyFile.txt";
+        }
+
+        public void CreateFile()
+        {
+            try
+            {
+                fs = new FileStream(filePath, FileMode.CreateNew);
+                fs.Close();
+               // fs.Dispose();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void WriteFile(string contents)
+        {
+            try
+            {
+                fs = new FileStream(filePath, FileMode.Open, FileAccess.Write);
+                StreamWriter sw = new StreamWriter(fs);
+                sw.Write(contents);
+                sw.Close();
+                sw.Dispose();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public string ReadFile()
+        {
+            string str = string.Empty;
+            try
+            {
+                fs = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+                StreamReader sr = new StreamReader(fs);
+                 str = sr.ReadToEnd();
+                sr.Close();
+                sr.Dispose();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return str;
+        }
+
+
+        public void Dispose()
+        {
+            fs.Dispose();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ CS_Stream/Program.cs
@@ -0,0 +1,23 @@
+﻿using CS_Stream;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("USing Stream");
+FileStreamOperation operation = new FileStreamOperation();
+try
+{
+
+    // operation.CreateFile();
+
+    // operation.WriteFile("I am from Stream");
+    string str =  operation.ReadFile();
+
+    Console.WriteLine(str);
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error: {ex.Message}");
+}
+finally
+{ 
+    operation.Dispose();
+}
+Console.ReadLine();
