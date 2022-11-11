4d9cbd12073ae9bade70c783198b39944713e4e0
maheshsabnis <sabnis_m@hotmail.com>
2022-11-11 12:24:45 +0530

    Date:-11-Nov-2022

---
+++ MVC_Apps/Controllers/CategoryProductController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Coditas.EComm.Entities;
+using Coditas.EComm.Repositories;
+
+namespace MVC_Apps.Controllers
+{
+    public class CategoryProductController : Controller
+    {
+        IDbRepository<Category, int> catRepo;
+        IDbRepository<Product, int> prdRepo;
+        public CategoryProductController(IDbRepository<Category, int> catRepo, IDbRepository<Product, int> prdRepo)
+        {
+            this.catRepo = catRepo;
+            this.prdRepo = prdRepo;
+        }
+
+        public async Task<IActionResult> Index(int? id)
+        {
+            List<Category> categories = null;
+            List<Product> products = null;
+            Tuple<List<Category>, List<Product>> tuple = null;
+            
+            categories = (await catRepo.GetAsync()).ToList();
+            
+            if (id == null || id == 0)
+            {
+                products = (await prdRepo.GetAsync()).ToList();
+            }
+            else
+            {
+                products = (await prdRepo.GetAsync()).Where(p => p.CategoryId == id).ToList();
+            }
+
+             tuple = new Tuple<List<Category>, List<Product>>(categories,products);
+
+            return View(tuple);
+        }
+
+        public IActionResult ShowDetails(int? id)
+        {
+            return RedirectToAction("Index", new { id = id });
+        }
+    }
+}
